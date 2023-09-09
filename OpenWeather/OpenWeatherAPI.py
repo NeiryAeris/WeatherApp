ca388e8b5cd7a614a00becd17d24adde4f1f6d2e
NeiryAeris <Kukorysenpai3506@gmail.com>
2023-09-09 15:20:46 +0700

    hiding API key

--- OpenWeather/OpenWeatherAPI.py
+++ OpenWeather/OpenWeatherAPI.py
@@ -1,5 +1,9 @@
+import os
 import requests
 import json
+from dotenv import load_dotenv
 
-API_KEY = "4d388f8ede3acfd4c0626bb4574c14e7"
-API_URL = f"http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API_KEY}"
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+API_URL = os.getenv('API_URL')
