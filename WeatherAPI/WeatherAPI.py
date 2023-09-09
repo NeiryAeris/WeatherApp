7406b22cc29b718eae5f9e030d4bd4d8804a5efe
NeiryAeris <Kukorysenpai3506@gmail.com>
2023-09-09 09:22:32 +0700

    idk what to put

---
+++ WeatherAPI/WeatherAPI.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+
+url = "https://weatherapi-com.p.rapidapi.com/forecast.json"
+
+querystring = {"q":"Hanoi","days":"3"}
+
+headers = {
+	"X-RapidAPI-Key": "1629509066msh5f709a8154fb897p18bf7fjsnd6e9edcd61f0",
+	"X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+my_data = response.json()
+json_filename = "api_data.json"
+
+with open(json_filename, 'w') as json_file:
+        json.dump(my_data, json_file, indent=4)
+# print(response.json())
