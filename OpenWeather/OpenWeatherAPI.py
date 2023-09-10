0af45dfc7bb2a6b25c8ff2e3f9aa7b7347e29fba
NeiryAeris <Kukorysenpai3506@gmail.com>
2023-09-11 05:04:27 +0700

    Update OpenWeatherAPI.py

--- OpenWeather/OpenWeatherAPI.py
+++ OpenWeather/OpenWeatherAPI.py
@@ -6,4 +6,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
-API_URL = os.getenv('API_URL')
+API_URL = f"http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API_KEY}"
