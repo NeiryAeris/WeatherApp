54f024fb197b0d2118fd3ed4b004ce369096433b
NeiryAeris <Kukorysenpai3506@gmail.com>
2023-09-12 06:52:45 +0700

    Finally got a good API

--- OpenWeather/OpenWeatherAPI.py
+++ OpenWeather/OpenWeatherAPI.py
@@ -6,4 +6,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
-API_URL = os.getenv('API_URL')
+API_URL = f"https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid={API_KEY}"
+
+response = requests.get(API_URL)
+
+my_data = response.json()
+json_filename = "data.json"
+
+with open(json_filename, 'w') as json_file:
+        json.dump(my_data, json_file, indent=4)
