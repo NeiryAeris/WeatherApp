8090af4e51ba7a6652d6ecaeee3f502a998438ee
NeiryAeris <Kukorysenpai3506@gmail.com>
2023-10-03 07:44:17 +0700

    nothing change

--- UITesting/display.py
+++ UITesting/display.py
@@ -3,18 +3,13 @@ import tkinter.messagebox
 import datetime
 import json
 
-
-# def read_hourly_weather_data(file_path):
-#     try:
-#         with open(file_path, "r") as file:
-#             data = json.load(file)
-#         return data
-#     except FileNotFoundError:
-#         return None
-
 file_name = "testing_data.json"
 
-
+def fetch_today(file_name):
+    weather = read_data(file_name)
+    unix_exchange = str(datetime.datetime.fromtimestamp(weather['current']['dt']))
+    return unix_exchange[9:11]
+    
 def read_data(file_name):
     try:
         with open(file_name, "r") as file:
@@ -33,10 +28,8 @@ def display_daily_weather():
         daily_text_box.delete(1.0, END)
         for entry in daily_weather:
             time = str(datetime.datetime.fromtimestamp(entry["dt"]))
-            daily_text_box.insert(
-                END,
-                f"{time[5:]}: {entry['temp']['day']}°K, {entry['weather'][0]['description']}\n",
-            )
+            daily_text_box.insert( END, f"{time[5:]}: {str(int(entry['temp']['day'])-273.15)[:4]}°C, {entry['weather'][0]['description']}\n",)
+
     else:
         daily_text_box.delete(1.0, END)
         daily_text_box.insert(END, "File not found or invalid JSON data.")
@@ -45,14 +38,13 @@ def display_daily_weather():
 def display_hourly_weather():
     weather = read_data(file_name)
     hourly_weather = weather["hourly"]
+    today = fetch_today(file_name)
     if hourly_weather:
         hourly_text_box.delete(1.0, END)
         for entry in hourly_weather:
             time = str(datetime.datetime.fromtimestamp(entry["dt"]))
-            hourly_text_box.insert(
-                END,
-                f"{time[5:]}: {entry['temp']}°K, {entry['weather'][0]['description']}\n",
-            )
+            if time[9:11] == today:
+                hourly_text_box.insert(END,f"{time[5:]}: {str(int(entry['temp'])-273.15)[:4]}°C, {entry['weather'][0]['description']}\n",)
     else:
         hourly_text_box.delete(1.0, END)
         hourly_text_box.insert(END, "File not found or invalid JSON data.")
@@ -112,13 +104,14 @@ footer_3 = Label(
 
 
 # Create widgets
-file_label = Label(app, text="Display:")
+daily_label = Label(app, text="Daily:")
+hourly_label = Label(app, text="Hourly:")
 # file_entry = Entry(app)
 get_weather_button = Button(
     app, text="Display Hourly Weather", command=display_hourly_weather
 )
-hourly_text_box = Text(app, width=45, height=10)
-daily_text_box = Text(app, width=45, height=8)
+hourly_text_box = Text(app, width=40, height=5)
+daily_text_box = Text(app, width=40, height=5)
 
 # Place widgets on the window------------------
 # file_label.pack()
@@ -126,8 +119,12 @@ daily_text_box = Text(app, width=45, height=8)
 # get_weather_button.pack()
 display_hourly_weather()
 display_daily_weather()
-daily_text_box.place(x=379, y=100)
-hourly_text_box.place(x=0, y=180)
+
+daily_label.place(x=398,y = 80)
+daily_text_box.place(x=399, y=100)
+
+hourly_label.place(x=398,y = 190)
+hourly_text_box.place(x=399, y=210)
 # result_text.grid(column=5)
 
 # Start the Tkinter main loop------------------
