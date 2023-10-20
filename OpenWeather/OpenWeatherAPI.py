f3d40137fccf00569d9ac0237b3768c5add303ad
NeiryAeris <Kukorysenpai3506@gmail.com>
2023-10-20 08:30:06 +0700

    call the api and store data

--- OpenWeather/OpenWeatherAPI.py
+++ OpenWeather/OpenWeatherAPI.py
@@ -2,13 +2,12 @@ import os
 import requests
 import json
 from dotenv import load_dotenv
-import tst
 from geopy.geocoders import Nominatim
 
 load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
-    
+
 def get_data(city_name):  
     geolocator = Nominatim(user_agent="city_to_lat_lon")
     location = geolocator.geocode(city_name)
@@ -19,6 +18,7 @@ def get_data(city_name):
     API_URL = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={API_KEY}"
     response = requests.get(API_URL)
     my_data = response.json()
-    json_filename = "testing_data.json"
+    json_filename = "data.json"
     with open(json_filename, 'w') as json_file:
         json.dump(my_data, json_file, indent=4)
+    print(API_URL)
--- OpenWeather/tst.py
+++ OpenWeather/tst.py
@@ -2,15 +2,7 @@ import os
 import requests
 import json
 from dotenv import load_dotenv
-
+import OpenWeatherAPI
 load_dotenv()
 
-API_KEY = os.getenv('API_KEY')
-API_URL = f"https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid={API_KEY}"
-
-# print(API_URL)
-# response = requests.get(API_URL)
-# print(response.json())
-city_data = json.load(open('location.json'))
-data = city_data[0]
-lat,lon = data["lat"],data["lon"]
+OpenWeatherAPI.get_data("hanoi")
